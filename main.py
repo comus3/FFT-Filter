ecffb4b093e9d6da1a754bf615807f9f1f9b59b1
comus3 <come.plantin.carrenard@gmail.com>
2023-09-25 15:33:11 +0200

    v.2.0

--- main.py
+++ main.py
@@ -6,23 +6,33 @@ import math
 from scipy.io import wavfile
 
 
-def filter(fft,cutoff,t):
-    filterFft = fft.copy()
-    filterFft[np.abs(np.fft.fftfreq(len(t))) > cutoff] = 0
-    filteredFft = fft * filterFft
-    filteredSignal = ifft(filteredFft)
-    return filteredSignal
+def filter(audio_data,cutoff,t):
+    audio_fft = fftLib.generateFFT(audio_data)
+    frequency_axis = np.fft.fftfreq(len(audio_data))
+    filter_fft = np.zeros(len(audio_data), dtype=np.complex128)
+    filter_fft[np.abs(frequency_axis) <= cutoff] = 1.0
+    
+    filtered_audio_fft = audio_fft * filter_fft
+    
+    filtered_audio_data = np.real(ifft(filtered_audio_fft))
+
+    return filtered_audio_data
+
 
 
 
 if __name__ == '__main__':
     startTime = 0.0
     sampleRate = 44100
-    func = lambda t, frequency:4 *np.sin((frequency*2*np.pi)*10/np.exp(frequency*t/10))
+    func = lambda t, frequency:6 * np.sin(t*np.sin(frequency*np.pi*2))
+    #func = lambda t, frequency:4 *np.sin((frequency*2*np.pi)*10/np.exp(frequency*t/10))
     import tkinter as tk
     from tkinter import messagebox
     def openOutput():
         import os
+        print("playing orignal sound")
+        os.startfile("original.wav")
+        print("playing final soundfile")
         os.startfile("output.wav")
     def generateWavButtonClicked():
         try:
@@ -30,18 +40,19 @@ if __name__ == '__main__':
             duration = float(duration_entry.get())
             cutoff = float(cutoff_entry.get())
             endTime = startTime+duration
+            sampleWidth = 4
             t = np.linspace(startTime, endTime, int((endTime - startTime) * sampleRate), endpoint=False)
             values = func(t, frequency)
             if 0 <= frequency <= 20000 and 0 <= duration <= 60 and cutoff <= 20000:
-                fftOfSignal = fftLib.generateFFT(values)
-                filteredSignal = filter(fftOfSignal,cutoff,t)
-                functowav.generateWav2(filteredSignal)
-                messagebox.showinfo("Success", "WAV file generated successfully!")
+                filteredSignal = filter(values,cutoff,t)
+                functowav.generateWavPydub(filteredSignal,sampleWidth,sampleRate)
+                functowav.generateWavPydub(values,sampleWidth,sampleRate,"original.wav")
                 openOutput()
+                messagebox.showinfo("Success", "WAV file generated and played successfully!")
             else:
                 messagebox.showerror("Error", "Invalid parameter values. Frequency must be in [0, 20 000] and duration in [0, 60] and cutoff in [0, 20 000].")
         except ValueError:
-            messagebox.showerror("Error", "Invalid input. Please enter valid numbers.")
+            messagebox.showerror("Error", "Could not generate wav... ):")
     root = tk.Tk()
     root.title("WAV Generator")
 
