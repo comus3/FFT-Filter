ba00d95b430abf718885b82758f4aecd7237f292
comus3 <come.plantin.carrenard@gmail.com>
2023-09-26 08:58:14 +0200

    added the plot signals fonctionaloty

--- main.py
+++ main.py
@@ -4,21 +4,79 @@ import matplotlib.pyplot as plt
 from scipy.fft import fft,ifft
 import math
 from scipy.io import wavfile
+import tkinter as tk
+from tkinter import messagebox
+
 
 
 def filter(audio_data,cutoff,t):
     audio_fft = fftLib.generateFFT(audio_data)
     frequency_axis = np.fft.fftfreq(len(audio_data))
     filter_fft = np.zeros(len(audio_data), dtype=np.complex128)
-    filter_fft[np.abs(frequency_axis) <= cutoff] = 1.0
-    
+    filter_fft[np.abs(frequency_axis) <= cutoff] = 0.5
     filtered_audio_fft = audio_fft * filter_fft
-    
     filtered_audio_data = np.real(ifft(filtered_audio_fft))
-
     return filtered_audio_data
 
 
+def openOutput(file):
+    import os
+    os.startfile(file)
+def playOriginal():
+    try:
+        openOutput("original.wav")
+    except ValueError:
+        messagebox.showerror("Error", "Could not read wav... ):")
+def playFiltered():
+    try:
+        openOutput("output.wav")
+    except ValueError:
+        messagebox.showerror("Error", "Could not read wav... ):")
+def plotSignals():
+    import matplotlib.pyplot as plt
+    plt.figure(figsize=(12, 6))
+
+    # Subplot 1: Original Signal
+    plt.subplot(2, 1, 1)
+    plt.plot(t, values, label='Original Signal', color='blue')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Amplitude')
+    plt.title('Original Signal')
+    plt.grid(True)
+    plt.legend()
+
+    # Subplot 2: Filtered Signal
+    plt.subplot(2, 1, 2)
+    plt.plot(t, filteredSignal, label='Filtered Signal', color='red')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Amplitude')
+    plt.title('Filtered signal')
+    plt.grid(True)
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+def generateWavButtonClicked():
+    try:
+        global values
+        global filteredSignal
+        global t
+        frequency = float(freq_entry.get())
+        duration = float(duration_entry.get())
+        cutoff = float(cutoff_entry.get())
+        endTime = startTime+duration
+        sampleWidth = 4
+        t = np.linspace(startTime, endTime, int((endTime - startTime) * sampleRate), endpoint=False)
+        values = func(t, frequency)
+        if 0 <= frequency <= 20000 and 0 <= duration <= 60 and cutoff <= 20000:
+            filteredSignal = filter(values,cutoff,t)
+            functowav.generateWavPydub(filteredSignal,sampleWidth,sampleRate)
+            functowav.generateWavPydub(values,sampleWidth,sampleRate,"original.wav")
+            messagebox.showinfo("Success", "WAV file generated and played successfully!")
+        else:
+            messagebox.showerror("Error", "Invalid parameter values. Frequency must be in [0, 20 000] and duration in [0, 60] and cutoff in [0, 20 000].")
+    except ValueError:
+        messagebox.showerror("Error", "Could not generate wav... ):")
 
 
 if __name__ == '__main__':
@@ -27,41 +85,7 @@ if __name__ == '__main__':
     #func = lambda t, frequency:6 * np.sin(t*np.sin(frequency*np.pi*2))
     #func = lambda t, frequency:4 *np.sin((frequency*2*np.pi)*10/np.exp(frequency*t/10))
     func = lambda t , frequency:7*np.tan(np.sin(t*frequency*2*np.pi))
-    import tkinter as tk
-    from tkinter import messagebox
-    def openOutput(file):
-        import os
-        os.startfile(file)
-    def playOriginal():
-        try:
-            openOutput("original.wav")
-        except ValueError:
-            messagebox.showerror("Error", "Could not read wav... ):")
-    def playFiltered():
-        try:
-            openOutput("output.wav")
-        except ValueError:
-            messagebox.showerror("Error", "Could not read wav... ):")
-
-    def generateWavButtonClicked():
-        try:
-            frequency = float(freq_entry.get())
-            duration = float(duration_entry.get())
-            cutoff = float(cutoff_entry.get())
-            endTime = startTime+duration
-            sampleWidth = 4
-            t = np.linspace(startTime, endTime, int((endTime - startTime) * sampleRate), endpoint=False)
-            values = func(t, frequency)
-            if 0 <= frequency <= 20000 and 0 <= duration <= 60 and cutoff <= 20000:
-                filteredSignal = filter(values,cutoff,t)
-                functowav.generateWavPydub(filteredSignal,sampleWidth,sampleRate)
-                functowav.generateWavPydub(values,sampleWidth,sampleRate,"original.wav")
-                openOutput()
-                messagebox.showinfo("Success", "WAV file generated and played successfully!")
-            else:
-                messagebox.showerror("Error", "Invalid parameter values. Frequency must be in [0, 20 000] and duration in [0, 60] and cutoff in [0, 20 000].")
-        except ValueError:
-            messagebox.showerror("Error", "Could not generate wav... ):")
+    
     root = tk.Tk()
     root.title("WAV Generator")
 
@@ -87,6 +111,8 @@ if __name__ == '__main__':
     generate_button = tk.Button(root, text="play original", command=playOriginal)
     generate_button.pack()
     generate_button = tk.Button(root, text="play filtered", command=playFiltered)
+    generate_button.pack()
+    generate_button = tk.Button(root, text="show signals", command=plotSignals)
     generate_button.pack()
 
     root.mainloop()
